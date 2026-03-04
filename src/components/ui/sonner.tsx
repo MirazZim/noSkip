ce564e41dc8749fb746751a5a3ff0a0ffb63b790
MirazZim <miraz.zim.38@gmail.com>
2026-03-04 10:44:51 +0600

    Feat: Fixed the toaster

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -9,12 +9,12 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      position="top-center"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg justify-center text-center",
+          description: "group-[.toast]:text-muted-foreground text-center",
           actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
